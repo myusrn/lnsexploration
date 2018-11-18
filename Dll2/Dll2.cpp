acbcd17e9cf6baa695c9fc8418a54abe6eaafb3c
robertob <ob1@outlook.com>
2018-11-17 20:15:15 -0800

    updates to clr changes

--- Dll2/Dll2.cpp
+++ Dll2/Dll2.cpp
@@ -3,4 +3,12 @@
 
 #include "stdafx.h"
 
+//#define DllImport   __declspec( dllimport ) 
+//#define DllExport   __declspec( dllexport ) 
+#define DllExport   extern "C" __declspec( dllexport )  
+//#define DllExport   extern "C"
 
+DllExport double DummyDllExportFunctionToCauseLibToBeCreated(double a, double b) 
+{
+	return a + b;
+}
--- Dll2/MathUtils.h
+++ Dll2/MathUtils.h
@@ -1,9 +1,24 @@
 #pragma once
 
+// c++/clr tutorial -> quick c++/cli - learn c++/cli in less than 10 minutes -> 
+// to create a managed class all you have to do is prefix the class definition with a protection modifier followed by ref, e.g. public ref class MyClass [ sealed ]
+// if you leave out ref keyword and compile with /clr switch the class shows up in c# managed code reference but none of the publics methods show up
+// where cli in "c++/cli" means c++ modified for common language infrastructure
+
+// c++/clr tutorial -> .net programming with c++/cli (visual c++) | native and .net interoperability | calling native functions from managed code | 
+// using c++ interop (implicit pinvoke) -> https://msdn.microsoft.com/en-us/library/2x8kf7zx.aspx
+
+// initially you get "Managed Debugging Assistant 'LoaderLock' : 'DLL 'fqdn path to dll' is attempting managed execution inside OS Loader lock. Do not attempt to run managed code
+// inside a DllMain or image initialization function since doing so can cause the application to hang.'" with a call to this class in place which can be made to go away using 
+// debug | exception settings | managed debugging assistants | loaderloack = checked -> unchecked after which you get the following
+
+//CnslApp.Dnf.Tests execution without a call to this class = The program '[21900] CnslApp.Dnf.Tests.exe' has exited with code 0 (0x0).
+//CnslApp.Dnf.Tests execution with a call to this class = The program '[27420] CnslApp.Dnf.Tests.exe' has exited with code -532462766 (0xe0434352).
+//MsTest.Dnf.Tests execution without a call to this class = The program '[21956] testhost.x86.exe' has exited with code 0 (0x0).
+//MsTest.Dnf.Tests execution with a call to this class = The program '[20804] testhost.x86.exe' has exited with code -532462766 (0xe0434352).
+
 namespace Dll2
 {
-	//class MathUtils
-	//public class MathUtils sealed  // w/o ref keyword none of the publics show up in managed code reference side of things
 	public ref class MathUtils sealed
 	{
 	private:
