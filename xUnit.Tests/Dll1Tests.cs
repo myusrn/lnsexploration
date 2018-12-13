5ee24be3234e50fce7ff3cc2e1befccf780d7f4a
robertob <ob1@outlook.com>
2018-12-13 08:43:23 -0800

    updating to test xUnit Theory that uses non-constant dataset

--- xUnit.Tests/Dll1Tests.cs
+++ xUnit.Tests/Dll1Tests.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -37,13 +36,6 @@ namespace xUnit.Tests
 //<Target Name="CopyToBin" BeforeTargets="Build"> <!-- for build and rebuild inclusion of dllimport referenced dll and appsettings -->
 //  <Copy SourceFiles="$(ProjectDir)appsettings.json" DestinationFolder="$(OutputPath)" />
 //</Target>
-
-        public enum AuthHeaderType
-        {
-            EasyAuth,
-            OAuth,
-            None
-        }
             
         [Theory]
         [ClassData(typeof(WebApiTestData))]
@@ -54,38 +46,6 @@ namespace xUnit.Tests
             Assert.NotNull(backendEndpoint);
             Assert.IsType<AuthHeaderType>(authHeaderType);
             Assert.NotNull(authHeaderValue);
-        }
-
-        public class WebApiTestData : IEnumerable<object[]>
-        {
-            static IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
-            //string backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndFuncAppEndpoint"];
-            //string backEndWebAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"];
-            //string backEndVmIisAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"];
-
-            //public WebApiTestData()
-            //{
-            //    var config = new ConfigurationBuilder()
-            //        //.SetBasePath(env.ContentRootPath) // adnc
-            //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //        //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true) // adnc, for example appsettings.Development.json
-            //        //.AddUserSecrets() // adnc, see http://go.microsoft.com/fwlink/?LinkID=532709 -> https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets
-            //        //.AddEnvironmentVariables() // adnc
-            //        .Build();
-
-            //    backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackFuncWebAppEndpoint"];
-            //    backEndWebAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"];
-            //    backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"];
-            //}
-
-            public IEnumerator<object[]> GetEnumerator()
-            {
-                yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndFuncAppEndpoint"], AuthHeaderType.EasyAuth, string.Empty };
-                yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"], AuthHeaderType.EasyAuth, string.Empty };
-                yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"], AuthHeaderType.OAuth, string.Empty };
-            }
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
 
         [Fact]
---
+++ xUnit.Tests/WebApiTestData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xUnit.Tests
+{
+    public enum AuthHeaderType
+    {
+        EasyAuth,
+        OAuth,
+        None
+    }
+
+    public class WebApiTestData : IEnumerable<object[]>
+    {
+        static IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+        //string backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndFuncAppEndpoint"];
+        //string backEndWebAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"];
+        //string backEndVmIisAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"];
+
+        //public WebApiTestData()
+        //{
+        //    var config = new ConfigurationBuilder()
+        //        //.SetBasePath(env.ContentRootPath) // adnc
+        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //        //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true) // adnc, for example appsettings.Development.json
+        //        //.AddUserSecrets() // adnc, see http://go.microsoft.com/fwlink/?LinkID=532709 -> https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets
+        //        //.AddEnvironmentVariables() // adnc
+        //        .Build();
+
+        //    backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackFuncWebAppEndpoint"];
+        //    backEndWebAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"];
+        //    backEndFuncAppEndpoint = config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"];
+        //}
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndFuncAppEndpoint"], AuthHeaderType.EasyAuth, string.Empty };
+            yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndWebAppEndpoint"], AuthHeaderType.EasyAuth, string.Empty };
+            yield return new object[] { config["DoglegCalculatorBackEndUnitTests:BackEndVmIisAppEndpoint"], AuthHeaderType.OAuth, string.Empty };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
